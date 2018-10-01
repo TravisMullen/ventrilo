25749bd27cf5e17766e4222f1577204b15060dc5
Travis Mullen <travis@boombangmedia.com>
2018-10-01 16:40:20 -0400

    stable v1.0.5

--- rollup.build.js
+++ rollup.build.js
@@ -7,12 +7,12 @@ import pkg from './package.json'
 export default {
   input: 'src/main.js',
   output: [
-    // { file: pkg.main, format: 'cjs' },
-    { file: pkg.main, format: 'es' }
+    { file: pkg.main, format: 'cjs' },
+    { file: pkg.module, format: 'es' }
   ],
   plugins: [
-    resolve(), // so Rollup can find `ms`
-    commonjs(), // so Rollup can convert `ms` to an ES module
+    resolve(),
+    commonjs(),
     json({
       // for tree-shaking, properties will be declared as
       // variables, using either `var` or `const`
--- test/custom-element.spec.js
+++ test/custom-element.spec.js
@@ -4,7 +4,7 @@
 
 import service from '..'
 
-const CUSTOM_ELEMENT = 'example-element'
+const CUSTOM_ELEMENT = 'research-element'
 const EXTENDED_ELEMENT = null
 const CUSTOM_EVENT_TYPE = 'example-event'
 const TEST_ELEMENT_ID = 'this-element-is-for-testing-only'
